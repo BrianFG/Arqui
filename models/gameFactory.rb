40fbe86f9e9536fb813153989649838855f65eae
Brian <brian.fg.94@gmail.com>
2016-05-05 12:31:54 -0500

    Documentacion

--- models/gameFactory.rb
+++ models/gameFactory.rb
@@ -1,36 +1,7 @@
 require ('./models/clases')
 
 
-
-
-
-def create_game (state, substate, money, life, fights, respect)
-  coin1 = CoinFlipper.new(0,"Encontraste una cartera, tira una moneda para probar tu suerte", 
-    Proc.new{|room, game| game.money += 10; room.go_sub(1); "Encontraste 10 varos"} , 
-    Proc.new{|room, game| room.go_sub(1); "No encontraste nada"}) 
-  coin2 = CoinFlipper.new(1,"Descubres una mochila, prueba tu suerte", 
-    Proc.new{|room, game| game.respect += 10; room.go_sub(2); "Encontraste una najava"} , 
-    Proc.new{|room, game| room.go_sub(2); "La mochila está vacia"})
-  fight1 = Fight.new(2, "Pelea con el cholo" , 
-    Proc.new{|room, game| game.life -= 10; room.go_sub(3); "pelea"} , 
-    Proc.new{|room, game| room.go_sub(3); "huir"})
-  move1  = Move.new(3, "Muevete" , 
-    Proc.new{|room, game| room.go_sub(1); "norte"} , 
-    nil,
-    Proc.new{|room, game| room.go_sub(3); "este"},
-    Proc.new{|room, game| room.go_sub(2); "oeste"} )
-
-
-  room1 = Room.new(0,"Despertasun letrero que dice Bienvenido a Ecatepec")
-  room1.state = coin1
-  room1.add_state(coin1).add_state(coin2).add_state(fight1).add_state(move1)
-  game = GameController.new(money,life,fights,respect)
-  game.state=room1
-  game.state.go_sub(substate)
-  game
-end
-
-
+#Method to build a GameController instance using the parameters state, substate, life, money, fights and respect
 def create_game2(state, substate, money, life, fights, respect)
 
   game = GameController.new(money,life,fights,respect)
